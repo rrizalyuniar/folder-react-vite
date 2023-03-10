1a738fdca9be9cd71fdc58aa9b200b233206f6c5
unknown <rizalyuniar123@gmail.com>
2023-03-11 03:18:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
